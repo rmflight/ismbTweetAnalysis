f90268c97e5a94c5cd052f40d7f394d81e8adc61
Robert Flight <r.flight79@gmail.com>
2014-07-25 12:08:40 -0400

    adding function to read ST's data

--- R/ismbTweetAnalysis-package.r
+++ R/ismbTweetAnalysis-package.r
@@ -21,3 +21,25 @@ NULL
 #' @source \url{https://github.com/neilfws/Twitter/tree/master/ismb/data}
 #' @docType data
 NULL
+
+#' read and process ST tweet data
+#' 
+#' reads in Stephen Turners archived twitter data and reformats it into a nicer format
+#' 
+#' @param file the file to process
+#' @param widths the widths of each field. Defaults based on ST's originals
+#' @return data.frame
+#' @importFrom stringr str_trim
+#' @export
+readTweetData <- function(file, hashSearch, widths=c(18, 14, 18, 1000)){
+  tweetData <- read.fwf(file, widths, stringsAsFactors=FALSE, comment.char="")
+  tweetData <- as.data.frame(sapply(tweetData, stringr::str_trim), stringsAsFactors=FALSE)
+  names(tweetData) <- c("id", "created", "screenName", "text")
+  
+  tweetData$screenName <- gsub("@", "", tweetData$screenName)
+  tweetData$created <- as.POSIXlt(tweetData$created, format="%b %d %H:%M")
+  tweetData <- tweetData[, c("text", "created", "id", "screenName")]
+  tweetData$hashSearch <- hashSearch
+  
+  return(tweetData)
+}
