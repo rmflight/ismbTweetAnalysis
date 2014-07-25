be314c3edb8953adc4351e90c2b038285cb78d3b
Robert Flight <r.flight79@gmail.com>
2014-07-25 11:23:51 -0400

    munging 2012 data a little, document it and save

--- R/ismbTweetAnalysis-package.r
+++ R/ismbTweetAnalysis-package.r
@@ -3,3 +3,21 @@
 #' @name ismbTweetAnalysis
 #' @docType package
 NULL
+
+
+#' ISMB tweets from 2012
+#' 
+#' A set of tweets over 5 days with the hashtag #ISMB from 2012. The variables are:
+#' 
+#' \itemize{
+#'   \item text. the text of the tweet
+#'   \item created. the date and time of the tweet
+#'   \item id. the twitter id of the tweet
+#'   \item screenName. who sent the tweet
+#' }
+#' 
+#' @format A data frame with 3162 rows and 4 variables
+#' @name ismb2012
+#' @source \url{https://github.com/neilfws/Twitter/tree/master/ismb/data}
+#' @docType data
+NULL
